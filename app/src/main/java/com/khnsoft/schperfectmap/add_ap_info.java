5225aa02e724a07c8cff70157af4de13dd644ecb
Park Kyu Hwon <svclaw2000@gmail.com>
2019-01-03 12:55:14 +0900

    Admin Interface: Search Wifi - DONE

--- app/src/main/java/com/khnsoft/schperfectmap/add_ap_info.java
+++ app/src/main/java/com/khnsoft/schperfectmap/add_ap_info.java
@@ -1,23 +1,36 @@
 package com.khnsoft.schperfectmap;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class add_ap_info extends AppCompatActivity {
     WifiManager wm;
-    List<ScanResult> scanResults;
+    List<ScanResult> scanResult;
     Button refresh;
+    String[] permissions = new String[] {Manifest.permission.ACCESS_COARSE_LOCATION};
+    public static final int MULTIPLE_PERMISSIONS = 10;
+    LinearLayout container;
+    int count;
+    int check;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,24 +38,32 @@ public class add_ap_info extends AppCompatActivity {
         setContentView(R.layout.activity_add_ap_info);
 
         wm = (WifiManager) getApplicationContext().getSystemService(WIFI_SERVICE);
-        if (!wm.isWifiEnabled() && wm.getWifiState() != WifiManager.WIFI_STATE_ENABLING) {
-            wm.setWifiEnabled(true);
-        }
 
         refresh = findViewById(R.id.refresh);
         refresh.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 wm.startScan();
+                count++;
+                Log.i("@@@", "Button clicked.");
             }
         });
+
+        count = 0;
+        check = 0;
+        container = findViewById(R.id.container);
     }
 
     @Override
     protected void onResume() {
-        IntentFilter intentFilter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
-        intentFilter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
-        registerReceiver(receiver, intentFilter);
+        if (wm != null) {
+            if (!wm.isWifiEnabled() && wm.getWifiState() != WifiManager.WIFI_STATE_ENABLING) {
+                wm.setWifiEnabled(true);
+            }
+            final IntentFilter filter = new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
+            filter.addAction(WifiManager.NETWORK_STATE_CHANGED_ACTION);
+            registerReceiver(receiver, filter);
+        }
         super.onResume();
     }
 
@@ -52,18 +73,76 @@ public class add_ap_info extends AppCompatActivity {
         super.onPause();
     }
 
-    BroadcastReceiver receiver = new BroadcastReceiver() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (!checkPermissions()) finish();
+        }
+    }
+
+    private BroadcastReceiver receiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
-                scanResults = wm.getScanResults();
-                for (int i=0; i<scanResults.size(); i++) {
-                    Log.i("@@@", String.format("SSID: %s", scanResults.get(i).SSID));
+            final String action = intent.getAction();
+            if (action != null) {
+                if (action.equals(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION)) {
+                    if (check != count)
+                    getWIFIScanResult();
+                    check = count;
+                } else if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
+                    context.sendBroadcast(new Intent("wifi.ON_NETWORK_STATE_CHANGED"));
                 }
-            } else if (action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)) {
-                sendBroadcast(new Intent("wifi.ON_NETWORK_STATE_CHANGED"));
             }
         }
     };
+
+    public void getWIFIScanResult() {
+        scanResult = wm.getScanResults();
+        container.removeAllViews();
+        if (scanResult.size()==0) {
+            no_ap no_one = new no_ap(add_ap_info.this);
+            container.addView(no_one);
+        } else {
+            for (int i = 0; i < scanResult.size(); i++) {
+                ScanResult result = scanResult.get(i);
+                Log.i("@@@", result.SSID + ": " + result.level + ", " + result.BSSID);
+
+                each_ap each = new each_ap(add_ap_info.this);
+                container.addView(each);
+                TextView ssid = findViewById(R.id.ssid);
+                TextView bssid = findViewById(R.id.bssid);
+                TextView level = findViewById(R.id.level);
+
+                if (result.SSID.length() > 15) ssid.setText(result.SSID.substring(0, 12)+"...");
+                else ssid.setText(result.SSID);
+                bssid.setText(result.BSSID);
+                level.setText("" + result.level);
+
+                int id_ssid = getResources().getIdentifier("ssid_"+i, "id", getApplicationContext().getPackageName());
+                int id_bssid = getResources().getIdentifier("bssid_"+i, "id", getApplicationContext().getPackageName());
+                int id_level = getResources().getIdentifier("level_"+i, "id", getApplicationContext().getPackageName());
+
+                ssid.setId(id_ssid);
+                bssid.setId(id_bssid);
+                level.setId(id_level);
+            }
+        }
+    }
+
+    boolean checkPermissions() {
+        int result;
+        List<String> listPermissionsNeeded = new ArrayList<>();
+        for (String p : permissions) {
+            result = ContextCompat.checkSelfPermission(add_ap_info.this, p);
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(p);
+            }
+        }
+        if (!listPermissionsNeeded.isEmpty()) {
+            ActivityCompat.requestPermissions(add_ap_info.this, listPermissionsNeeded.toArray(new String[listPermissionsNeeded.size()]), MULTIPLE_PERMISSIONS);
+            return false;
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/khnsoft/schperfectmap/drawer_main.java
+++ app/src/main/java/com/khnsoft/schperfectmap/drawer_main.java
@@ -1,5 +1,7 @@
 package com.khnsoft.schperfectmap;
 
+import android.content.ClipData;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -15,6 +17,7 @@ import android.view.MenuItem;
 
 public class drawer_main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,6 @@ public class drawer_main extends AppCompatActivity
         setContentView(R.layout.activity_drawer_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,18 +74,12 @@ public class drawer_main extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        if (id == R.id.user_interface) {
+            Intent intent = new Intent(drawer_main.this, user_interface.class);
+            startActivity(intent);
+        } else if (id == R.id.admin_interface) {
+            Intent intent = new Intent(drawer_main.this, add_ap_info.class);
+            startActivity(intent);
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
